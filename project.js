7a199f6b79a65933cb51d0da3b58f5fab6df6a55
iampaul783 <iampaul783@gmail.com>
2015-11-24 00:27:35 +0800

    fix bootstrap & datas

--- project.js
+++ project.js
@@ -1,5 +1,5 @@
 angular.module('project', ['ngRoute', 'firebase'])
- 
+
 .value('fbURL', 'https://ng-projects-list.firebaseio.com/')
 .service('fbRef', function(fbURL) {
   return new Firebase(fbURL)
@@ -20,7 +20,7 @@ angular.module('project', ['ngRoute', 'firebase'])
       return deferred.promise;
   }
 })
- 
+
 .service('Projects', function($q, $firebase, fbRef, fbAuth) {
   var self = this;
   this.fetch = function () {
@@ -31,26 +31,26 @@ angular.module('project', ['ngRoute', 'firebase'])
       var $projects = $firebase(ref);
       ref.on('value', function(snapshot) {
         if (snapshot.val() === null) {
-          $projects.$set(window.projectsArray);
+          $projects.$set([{"name":"AngularJS","site":"http://angularjs.org","description":"HTML enhanced for web apps!"},{"name":"jQuery","site":"http://jquery.com/","description":"Write less, do more."},{"name":"Backbone","site":"http://documentcloud.github.com/backbone/","description":"Models for your apps."},{"name":"SproutCore","site":"http://sproutcore.com/","description":"Innovative web-apps."},{"name":"Sammy","site":"http://sammyjs.org/","description":"Small with class."},{"name":"Spine","site":"http://spinejs.com/","description":"Awesome MVC Apps."},{"name":"Cappucino","site":"http://cappuccino.org/","description":"Objective-J."},{"name":"Knockout","site":"http://knockoutjs.com/","description":"MVVM pattern."},{"name":"GWT","site":"https://developers.google.com/web-toolkit/","description":"JS in Java."},{"name":"Ember","site":"http://emberjs.com/","description":"Ambitious web apps."},{"name":"Batman","site":"http://batmanjs.org/","description":"Quick and beautiful."}]);
         }
         self.projects = $projects.$asArray();
         deferred.resolve(self.projects);
       });
- 
+
       //Remove projects list when no longer needed.
       ref.onDisconnect().remove();
       return deferred.promise;
     });
   };
 })
- 
+
 .config(function($routeProvider) {
   var resolveProjects = {
     projects: function (Projects) {
       return Projects.fetch();
     }
   };
- 
+
   $routeProvider
     .when('/', {
       controller:'ProjectListController as projectList',
@@ -71,12 +71,12 @@ angular.module('project', ['ngRoute', 'firebase'])
       redirectTo:'/'
     });
 })
- 
+
 .controller('ProjectListController', function(projects) {
   var projectList = this;
   projectList.projects = projects;
 })
- 
+
 .controller('NewProjectController', function($location, projects) {
   var editProject = this;
   editProject.save = function() {
@@ -85,23 +85,23 @@ angular.module('project', ['ngRoute', 'firebase'])
       });
   };
 })
- 
+
 .controller('EditProjectController',
   function($location, $routeParams, projects) {
     var editProject = this;
     var projectId = $routeParams.projectId,
         projectIndex;
- 
+
     editProject.projects = projects;
     projectIndex = editProject.projects.$indexFor(projectId);
     editProject.project = editProject.projects[projectIndex];
- 
+
     editProject.destroy = function() {
         editProject.projects.$remove(editProject.project).then(function(data) {
             $location.path('/');
         });
     };
- 
+
     editProject.save = function() {
         editProject.projects.$save(editProject.project).then(function(data) {
            $location.path('/');
